97e51ec39ac84605f121d30eb9e9b269a0fa4971
Your Name <you@example.com>
2025-12-05 12:11:17 +0100

    cancel sfc mode

--- projects/main-app/src/app/feature/characters/models/character.model.ts
+++ projects/main-app/src/app/feature/characters/models/character.model.ts
@@ -1,4 +1,14 @@
 export interface Character {
   id: number;
   name: string;
+  height?: number;
+  mass?: number;
 }
+
+export type AddingCharacter = Omit<Character, 'id'> & { id?: number };
+
+export const createDefaultCharacter = (): AddingCharacter => ({
+  name: 'Unnamed Character',
+  mass: 50,
+  height: 100
+});
